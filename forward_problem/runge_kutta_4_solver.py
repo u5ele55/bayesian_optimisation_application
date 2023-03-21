cfae93aad967d00796c74635c4faa0e9d8efa892
Shaganov Vyacheslav <slavikshag@gmail.com>
2023-03-21 22:49:32 +0300

    solver method to get all states up to some point implemented

--- forward_problem/runge_kutta_4_solver.py
+++ forward_problem/runge_kutta_4_solver.py
@@ -2,12 +2,12 @@ from forward_problem.solver import AbstractForwardSolver
 
 class RungeKutta4Solver(AbstractForwardSolver):
     ''' Concrete solver for forward problem. Implements RK4 method '''
-    def __init__(self, system, step=0.01, store_values=True):
-        super().__init__(system, step, store_values)
+    def __init__(self, system, step=0.01):
+        super().__init__(system, step)
         self.k_1 = self.k_2 = self.k_3 = self.k_4 = 0
 
     def method_step(self, state, step: float):
-        ''' Solves problem with RK4 method '''
+        ''' Implementation of RK4 method step '''
         self.k_1 = self.system.f(state)
         self.k_2 = self.system.f(state + step * self.k_1 / 2)
         self.k_3 = self.system.f(state + step * self.k_2 / 2)
--- forward_problem/solver.py
+++ forward_problem/solver.py
@@ -4,46 +4,35 @@ class AbstractForwardSolver:
     ''' Abstract forward problem solver, has implemented `get_state` 
     method for runge-kutta methods and unimplemented `method_step` 
     method (has to be implemented in concrete solvers)'''
-    def __init__(self, system, step = 1e-2, store_values = True):
+    def __init__(self, system, step = 1e-2):
         self.step = step
         self.system = system
-        self.store_values = store_values
-
-        if self.store_values:
-            self.values = {0: system.initial_state}
-            self.last_evaluated_moment = 0
 
     def get_state(self, time: float):
         ''' Returns state in time = `t` '''
-        whole_part = self.step * int(time / self.step)
-        
-        if self.store_values and time <= self.last_evaluated_moment:
-            # recalculate more precisely
-            time_diff = time - whole_part
-            precise_state = self.method_step(self.values[whole_part], time_diff)
-            return precise_state
-        
-        if self.store_values:
-            state = self.values[self.last_evaluated_moment]
-            start_moment = int(self.last_evaluated_moment / self.step) + 1
-        else:
-            state = self.system.initial_state
-            start_moment = 1
+        state = self.system.initial_state
 
-        for i in range(start_moment, int(time / self.step) + 1):
+        for _ in range(1, int(time / self.step) + 1):
             state = self.method_step(state, self.step)
-            if self.store_values:
-                self.values[self.step * i] = state
         
-        if self.store_values:
-            self.last_evaluated_moment = int(time / self.step) * self.step
-
         # recalculate more precisely for case when t != self.h * k for some whole k
+        whole_part = self.step * int(time / self.step)
         time_diff = time - whole_part
         if time_diff != 0:
             precise_state = self.method_step(state, time_diff)
             return precise_state
         return state
+
+    def get_states(self, time: float) -> list:
+        ''' Returns map of states up to nearest to `time` whole multiple of `step` '''
+        values = {}
+        state = self.system.initial_state
+
+        for i in range(1, int(time / self.step) + 1):
+            state = self.method_step(state, self.step)
+            values[self.step * i] = state
+
+        return values
 
     # factory method
     def method_step(self, state, step: float):

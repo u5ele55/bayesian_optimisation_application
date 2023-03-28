a157d819f4e137a5c1f6204b6b6d2ca0b5d50ec3
Shaganov Vyacheslav <slavikshag@gmail.com>
2023-03-28 17:22:25 +0300

    most of the functions now change state vector instead of creating new one reallocations minimized numpy integrated

--- forward_problem/solver.py
+++ forward_problem/solver.py
@@ -1,4 +1,5 @@
 ''' Abstract forward solver '''
+import numpy as np
 
 class AbstractForwardSolver:
     ''' Abstract forward problem solver, has implemented `get_state` 
@@ -7,35 +8,37 @@ class AbstractForwardSolver:
     def __init__(self, system, step = 1e-2):
         self.step = step
         self.system = system
+        self.state = np.array([0,0])
 
     def get_state(self, time: float):
-        ''' Returns state in time = `t` '''
-        state = self.system.initial_state
+        ''' Returns state in time = `time` '''
+        self.state = np.copy(self.system.initial_state)
 
         for _ in range(1, int(time / self.step) + 1):
-            state = self.method_step(state, self.step)
-        
+            self.method_step(self.state, self.step)
+
         # recalculate more precisely for case when t != self.h * k for some whole k
         whole_part = self.step * int(time / self.step)
         time_diff = time - whole_part
-        if time_diff != 0:
-            precise_state = self.method_step(state, time_diff)
-            return precise_state
-        return state
 
-    def get_states(self, time: float) -> list:
+        if time_diff != 0:
+            self.method_step(self.state, time_diff)
+
+        return self.state
+
+    def get_states(self, time: float) -> dict[float, np.ndarray]:
         ''' Returns map of states up to nearest to `time` whole multiple of `step` '''
         values = {}
-        state = self.system.initial_state
+        self.state = np.copy(self.system.initial_state)
 
         for i in range(1, int(time / self.step) + 1):
-            state = self.method_step(state, self.step)
-            values[self.step * i] = state
+            self.method_step(self.state, self.step)
+            values[self.step * i] = np.copy(self.state)
 
         return values
 
     # factory method
-    def method_step(self, state, step: float):
+    def method_step(self, state: np.ndarray, step: float):
         ''' One step of the method '''
         raise NotImplementedError("Use concrete forward problem solver")
     

3d9eb2abdde73a1f3c181b87cdf4dfed4b0fe734
Shaganov Vyacheslav <slavikshag@gmail.com>
2023-04-26 12:11:58 +0300

    0JLRgdGRINGD0L3QuNGH0YLQvtC20LXQvdC+

--- bayesian_optimization/utils/acquisition_functions.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-
-def acq_ucb(values, mu, div, coef=1.0):
-    argmin = np.argmin(mu - coef * div)
-    return values[argmin]
--- main.py
+++
@@ -1,7 +0,0 @@
-''' Entry point '''
-import system_noise.testing
-import bayesian_optimization.testing
-
-if __name__ == '__main__':
-    # system_noise.testing.test()
-    bayesian_optimization.testing.test_1d()
